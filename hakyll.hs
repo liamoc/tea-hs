80672ad77a1dc674d4eaa2336c395c8a2803c525
Liam O'Connor-Davis <liamoc@cse.unsw.edu.au>
2010-01-16 20:36:53 +1100

    Completed Design adding page
    nicer
    
    nicer
    
    css fixes
    
    more stuff
    
    added highlighting
    
    more
    
    syntax fixes
    
    syntax contrast
    
    Completing Design
    more fixes
    
    new css
    
    cooler syntax areas
    
    more borders
    
    More fixes
    
    trying again
    
    updated sidebar
    
    more fixes to css
    
    css fixes
    
    more fixes
    
    more fixes
    
    png -> gif
    
    updated main.css
    
    updating px->em
    
    redesign
    
    lightening
    
    recoloring again
    
    slight updates for sizing rules

--- hakyll.hs
+++ hakyll.hs
@@ -1,11 +1,26 @@
-#!/usr/bin/runhaskell
+#!/usr/bin/env runhaskell
+
 import Text.Hakyll (hakyll)
 import Text.Hakyll.Render (css, static, renderChain)
-import Text.Hakyll.Renderables (createPagePath)
+import Text.Hakyll.Renderables (createCustomPage)
 import Text.Hakyll.File (directory)
+import Text.Hakyll.Page (readPage, getBody, getValue)
+import System.FilePath
+import Control.Applicative((<$>))
 
 main = hakyll $ do
          directory css "css"
          directory static "static"
-         directory render "pages"         
-  where render = renderChain ["templates/default.html"] . createPagePath
+         flip directory "pages" $ \path -> do 
+            if takeExtension path == ".pdc" 
+              then do
+              page <- readPage path
+              renderChain ["templates/default.html"] $
+                createCustomPage (takeBaseName path <.> "html") 
+                                 [ path , path <.> "side" ]
+                                 [ ("body", Left $ getBody page)
+                                 , ("sidebar", Right $ getBody <$> readPage (path <.> "side"))
+                                 , ("title", Left $ getValue "title" page)
+                                 ]
+              return ()
+              else return ()
